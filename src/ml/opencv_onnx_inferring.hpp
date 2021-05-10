b4647d40b6ef5e065d04f4c7b178d8844b2a5e73
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-05-10 22:16:39 +0800

    Remove cv_version

--- src/ml/opencv_onnx_inferring.hpp
+++ src/ml/opencv_onnx_inferring.hpp
@@ -1,57 +1,48 @@
 #pragma once
 
+#include <vector>
+#include <string>
+#include <utility>
 #include <iostream>
 
 #include <opencv2/core.hpp>
 #include <opencv2/dnn/dnn.hpp>
 
-namespace ooi {
+namespace opi {
     inline static void cv_version(void) {
-        std::cout << "[OOI] opencv version: " << cv::getVersionString() << std::endl;
+        std::cout << "[OPI] opencv version: " << cv::getVersionString() << std::endl;
     }
     
     class model {
     public:
-        inline model(const char* onnx_model_path, const cv::Size& input_size) {
-            model::load(onnx_model_path);
+        inline model(const char* model_path, const cv::Size kernel_size, const char* input_layer_name, const char* output_layer_name) {
+            model::load(model_path);
             model::layers();
             
-            this->input_size = input_size;
+            this->kernel_size       = kernel_size;
+            this->input_layer_name  = input_layer_name;
+            this->output_layer_name = output_layer_name;
         }
         
-        inline int inferring(const cv::Mat& input) {
-            cv::resize(input, tmp, input_size);
+        inline cv::Mat&& inferring(const cv::Mat& input) {
+            cv::resize(input, tmp, kernel_size);
             cv::cvtColor(tmp, tmp, cv::COLOR_BGR2GRAY);
             cv::threshold(tmp, tmp, 0, 255, cv::THRESH_BINARY | cv::THRESH_OTSU);
             
-            opencv_net.setInput(cv::dnn::blobFromImage(tmp));
+            opencv_net.setInput(cv::dnn::blobFromImage(tmp, 1, tmp.size(), cv::Scalar(), false), input_layer_name);
             
-            float max_probability     { 0 };
-            int   max_probability_idx { 0 };
-            int   i                   { -1 };
-            opencv_net.forward().forEach<float>([&max_probability, &max_probability_idx, &i](float &data, [[maybe_unused]] const int * position) -> void {
-                if (++i) {
-                    if (data > max_probability) {
-                        max_probability     = data;
-                        max_probability_idx = i;
-                    }
-                } else {
-                    max_probability = data;
-                }
-            });
-            
-            return max_probability_idx;
+            return std::move(opencv_net.forward(output_layer_name).reshape(1, 1));
         }
         
     protected:
-        inline void load(const char* onnx_model_path) {
-            model_path = onnx_model_path;
-            opencv_net = cv::dnn::readNetFromONNX(model_path);
+        inline void load(const char* model_path) {
+            this->model_path = model_path;
+            opencv_net = cv::dnn::readNetFromTensorflow(this->model_path);
             
             if (!opencv_net.empty()) {
-                std::cout << "[OOI] load model success: " << model_path << std::endl;
+                std::cout << "[OI] load model success: " << this->model_path << std::endl;
             } else {
-                std::cout << "[OOI] load model failed: " << model_path << std::endl;
+                std::cout << "[OPI] load model failed: " << this->model_path << std::endl;
                 return;
             }
             
@@ -64,11 +55,12 @@ namespace ooi {
         
         inline void layers(void) {
             if (opencv_net.empty()) {
-                std::cout << "[OOI] model is empty" << std::endl;
+                std::cout << "[OPI] model is empty" << std::endl;
                 return;
             }
             
-            std::cout << "[OOI] model from " << model_path << " has layers: " << std::endl;
+            std::cout << "[OPI] model from path: " << this->model_path << std::endl;
+            std::cout << "[OPI] model layers: " << std::endl;
             for (const auto& layer_name : opencv_net.getLayerNames()) {
                 std::cout << "\t\t" << layer_name << std::endl;
             }
@@ -78,7 +70,10 @@ namespace ooi {
         cv::dnn::Net opencv_net;
         const char*  model_path;
         
-        cv::Size     input_size;
+        const char*  input_layer_name;
+        const char*  output_layer_name;
+        
+        cv::Size     kernel_size;
         cv::Mat      tmp;
     };
 }
