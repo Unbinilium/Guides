568dd1482f268999f7d49a1b692dee30f4662ce9
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-05-12 20:41:38 +0800

    Update example_for_ringbuffer.cpp

--- src/ringbuffer/example_for_ringbuffer.cpp
+++ src/ringbuffer/example_for_ringbuffer.cpp
@@ -3,9 +3,9 @@
 #include "ringbuffer.hpp"
 
 int main() {
-    ubn::ringbuffer<size_t, 3> rb;
+    ubn::ringbuffer<std::size_t, 3> rb;
     
-    for(size_t i = 1; i != 10; ++i) {
+    for(std::size_t i = 1; i != 10; ++i) {
         if (rb.push_head(i)) {
             std::cout << "get tail from filled ringbuffer " << rb.catch_tail() << " while pusing -> " << i << std::endl;
         } else {
