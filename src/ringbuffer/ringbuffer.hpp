f7ea2f200bf3f97616b047e4756411b9ce7e8339
Unbinilium <15633984+Unbinilium@users.noreply.github.com>
2021-05-12 20:40:02 +0800

    Update ringbuffer.hpp

--- src/ringbuffer/ringbuffer.hpp
+++ src/ringbuffer/ringbuffer.hpp
@@ -14,15 +14,15 @@
 #include <type_traits>
 
 namespace ubn {
-    template<typename T, const size_t capacity>
+    template<typename T, const std::size_t capacity>
     class ringbuffer {
     public:
         /*
          * @brief: Initialize ringbuffer class
          * @param: typename, the typename of item(s) for ringbuffer to store
-         * @param: const size_t, the capacity of ringbuffer, overwrites the old item(s) when pushing if ringbuffer is full
+         * @param: const std::size_t, the capacity of ringbuffer, overwrites the old item(s) when pushing if ringbuffer is full
          */
-        inline ringbuffer(void) {
+        constexpr inline ringbuffer(void) {
             static_assert(capacity >= 1UL, "ringbuffer capacity < 1");
         }
         
@@ -31,8 +31,8 @@ namespace ubn {
          * @param:  const T&, const lvalue reference of typename T
          * @return: bool, whether you're pushing on a full ringbuffer which performs the overwrite action on an old item
          */
-        inline bool push_head(const T& v) noexcept {
-            m_buffer[m_position++ % capacity] = v;
+        constexpr inline bool push_head(const T& __v) noexcept {
+            m_buffer[m_position++ % capacity] = __v;
             return is_overwrite();
         }
         
@@ -42,8 +42,8 @@ namespace ubn {
          * @return: bool, whether you're pushing on a full ringbuffer which performs the overwrite action on an old item
          */
         template<typename T_ = T>
-        inline bool push_head(T&& v, typename std::enable_if<!std::is_reference<T_>::value, std::nullptr_t>::type = nullptr) noexcept {
-            m_buffer[m_position++ % capacity] = std::forward<T>(v);
+        constexpr inline bool push_head(T&& __v, typename std::enable_if<!std::is_reference<T_>::value, std::nullptr_t>::type = nullptr) noexcept {
+            m_buffer[m_position++ % capacity] = std::forward<T>(__v);
             return is_overwrite();
         }
         
@@ -51,15 +51,15 @@ namespace ubn {
          * @brief:  Get an item from ringbuffer tail, return the default initialized type T if ringbuffer is empty, and keeps the buffer in the ringbuffer if there had no overwrite action has been performed
          * @return: T, the item from ringbuffer tail or the default initialed type T if ringbuffer is empty
          */
-        inline T catch_tail(void) noexcept {
+        constexpr inline T catch_tail(void) noexcept {
             return m_buffer[is_empty() ? capacity : (m_position + (m_capacity != capacity ? m_capacity++ : capacity) - capacity) % capacity];
         }
         
         /*
          * @brief:  Get current catchable item counts from ringbuffer
-         * @return: size_t, the current catchable item counts from ringbuffer
+         * @return: std::size_t, the current catchable item counts from ringbuffer
          */
-        inline size_t size(void) noexcept {
+        constexpr inline std::size_t size(void) noexcept {
             return capacity - m_capacity;
         }
         
@@ -67,7 +67,7 @@ namespace ubn {
          * @brief:  Check if ringbuffer is empty, empty for true, otherwise for false
          * @return: bool
          */
-        inline bool is_empty(void) noexcept {
+        constexpr inline bool is_empty(void) noexcept {
             return m_capacity != capacity ? false : true;
         }
         
@@ -75,26 +75,26 @@ namespace ubn {
          * @brief:  Check if ringbuffer is full, full for true, otherwise for false
          * @return: bool
          */
-        inline bool is_full(void) noexcept {
+        constexpr inline bool is_full(void) noexcept {
             return m_capacity ? false : true;
         }
         
         /*
          * @brief: Empty all buffer(s) inside ringbuffer, not free the memory, only reset the ringbuffers pointer position
          */
-        inline void empty(void) noexcept {
+        constexpr inline void empty(void) noexcept {
             m_capacity = capacity;
             m_position = 0;
         }
         
     protected:
-        inline bool is_overwrite(void) noexcept {
+        constexpr inline bool is_overwrite(void) noexcept {
             return (m_capacity ? --m_capacity : 0) ? false : true;
         }
         
     private:
-        T      m_buffer   [ capacity + 1 ];
-        size_t m_capacity { capacity };
-        size_t m_position { 0 };
+        T           m_buffer   [ capacity + 1 ];
+        std::size_t m_capacity { capacity };
+        std::size_t m_position { 0 };
     };
 }
